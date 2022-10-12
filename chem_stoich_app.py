bc7a59008911ced4444ac67bbd28166a214870d6
John Paul M. Curada <84607933+JpCurada@users.noreply.github.com>
2022-10-12 21:32:33 +0800

    Update chem_stoich_app.py

--- chem_stoich_app.py
+++ chem_stoich_app.py
@@ -1,4 +1,3 @@
-import mendeleev as md
 from pyvalem.formula import Formula
 from pyvalem.reaction import Reaction, ReactionParseError
 import molmass as mm
@@ -105,17 +104,20 @@ def mass_to_number_of_particles(reaction_substance_mole_dict, reaction_substance
 def number_of_particles_to_mass(reaction_substance_mole_dict, reaction_substance_molar_mass_dict, nop_amount, nop_substance, determine_substance_mass):
     return nop_amount * (1/(6.022 *(10**23))) * (reaction_substance_mole_dict[determine_substance_mass]/reaction_substance_mole_dict[nop_substance]) * (reaction_substance_molar_mass_dict[determine_substance_mass])
 
-st.title('S-KAPNAYAN: A Web App for Stoichiometry Problems')
-
+st.title('DA-KAPNAYAN')
+st.write("""###### A web application for solving problems involving Stoichiometry with Dimensional Analysis""")
+st.caption("Stoichiometry is a tough subject for many pupils. The traditional approach of linking chemical quantities in a reaction via dimensional analysis includes a number of phases in which students struggle to determine which conversion factor to develop and how to apply it.")
 # st.sidebar.success("S-KAPNAYAN")
-
+st.markdown('---')
+st.subheader("Chemical Equation")
 reaction = st.text_input('Input a Balanced Chemical Equation:')
 
+st.caption("This input bar is case-sensitive. You should put a space in every element and you must only use this arrow('→') to separate reactants and products.")
 
-with st.expander("Check these examples!"):
+with st.expander("Try these examples!"):
     st.write("""
-    #### You can try this balanced chemical equations ^_^
-    ##### Just copy and paste it to the input bar.
+    ##### You can try this balanced chemical equations ^_^
+    Just copy and paste it to the input bar.
     1. 6CO2 + 6H2O → C6H12O6 + 6O2
     2. SiCl4 + 4H2O → H4SiO4 + 4HCl
     3. 2Al + 6HCl → 2AlCl3 + 3H2
@@ -153,7 +155,10 @@ try:
 
     st.markdown("---")
 
-    st.header('Stoichiometry')
+    st.header('Dimensional Analysis')
+    st.markdown("Note:")
+    st.caption("1. Units of mass is represented only by grams(g)\n2. Units of mass is represented only by mole(mol)\n3. Units of number of particles can be represented as atoms, molecules, and particles\n4. 'e+' is equal to 'x10' and its following number is an exponent")
+
     feature_t1, feature_t2, feature_t3, feature_t4, feature_t5, feature_t6, feature_t7, feature_t8 = st.tabs(['Mole to Mole', 'Mass to Mass','Mass to Mole', 'Mole to Mass', 'Mole to Number of Particles', 'Number of Particles to Mole', 'Mass to Number of Particles', 'Number of Particles to Mass'])  
     
     with feature_t1:
@@ -165,7 +170,7 @@ try:
         mol_amount = st.number_input('Amount of given substance in moles:', value=1.00, key="mole_to_mole_amount")
 
         st.write("""##### Required: """)
-        determine_substance_mol = st.selectbox("Find amount the mole of this substance:", tuple(reaction_substance_list), key="mole_to_mole_required")
+        determine_substance_mol = st.selectbox("Find the amount the mole of this substance:", tuple(reaction_substance_list), key="mole_to_mole_required")
 
         if mol_substance == determine_substance_mol:
             st.error('Substance in Given must NOT BE EQUAL to Substance in Required. ')
@@ -259,7 +264,6 @@ try:
 
     with feature_t5:
         st.subheader('Mole-to-Particles Conversion')
-        st.caption('Units of number of particles can be represented as atoms, molecules, and particles')
 
         st.latex(Reaction(reaction).latex)
 
@@ -286,7 +290,6 @@ try:
 
     with feature_t6:
         st.subheader('Particles-to-Mole Conversion')
-        st.caption('Units of number of particles can be represented as atoms, molecules, and particles')
 
         st.latex(Reaction(reaction).latex)
 
@@ -312,7 +315,6 @@ try:
 
     with feature_t7:
         st.subheader('Mass-to-Particles Conversion')
-        st.caption('Units of number of particles can be represented as atoms, molecules, and particles')
 
         st.latex(Reaction(reaction).latex)
 
@@ -339,7 +341,6 @@ try:
 
     with feature_t8:
         st.subheader('Particles-to-Mass Conversion')
-        st.caption('Units of number of particles can be represented as atoms, molecules, and particles')
 
         st.latex(Reaction(reaction).latex)
 
@@ -367,3 +368,16 @@ except ReactionParseError:
     pass
 
 st.markdown('---')
+
+personal_col, reference_col = st.columns(2)
+
+with personal_col:
+    st.subheader('About the Creator')
+    st.caption('"_Exploring Python modules and maximizing the potential of Streamlit._"')
+    st.caption("_— John Paul M. Curada | Python and Data Science Enthusiast_")
+    st.write("**Contacts:**")
+    st.caption("For feedbacks and inquires, message me at: ")
+    st.caption("Email: j.curada02@gmail.com")
+    st.caption("Twitter: https://twitter.com/jpcodesss_")
+
+
