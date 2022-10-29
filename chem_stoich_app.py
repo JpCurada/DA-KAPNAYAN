a2e3a2fb91f02466395c78b4b7ccace2c6fa84fc
John Paul M. Curada <84607933+JpCurada@users.noreply.github.com>
2022-10-29 19:36:24 +0800

    Update chem_stoich_app.py

--- chem_stoich_app.py
+++ chem_stoich_app.py
@@ -22,6 +22,7 @@ st.set_page_config(
     
 hide_streamlit_style = """
             <style>
+            #MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
             </style>
             """
